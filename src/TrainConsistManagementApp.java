734a5e7eac05a6cc05be5590178170faeff71167
parnikaaa06 <puranikparnika06@gmail.com>
2026-03-25 14:44:31 +0530

    UC1: Display Consist Summary

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrainConsistManagementApp {
+
+
+    static List<String> bogies = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        displaySummary();
+    }
+
+    public static void displaySummary() {
+        System.out.println("Initial Bogie Count: " + bogies.size());
+    }
 }
